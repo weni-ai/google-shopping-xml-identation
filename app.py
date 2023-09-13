73789f7d16a5e3445d3b569d8438ef6eb7216a67
John Cordeiro <johncordeiro@MacBook-Air-de-John.local>
2023-09-13 09:51:38 -0300

    Fix hostname

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def get_indented_xml():
         return f"An error occurred: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
